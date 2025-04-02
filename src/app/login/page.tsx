36fffb9b0ad2214fa2665f2e9d10f5dbe38277b7
Omar Mneimneh <omarmneimneh2000@gmail.com>
2025-04-01 21:04:23 -0400

    need to figure out authentication and some styling stuff

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
 import {Header, Footer} from "@/src/app/components/lib/Layout"
+import SignInScreen from '@/src/app/components/auth/SignInScreen';
+import { Sign } from "crypto";
 export default function Login(){
     return(
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex items-center justify-center flex-grow space-x-2">
+            {/* <main className="flex items-center justify-center flex-grow space-x-2">
                 <p>Under Development</p>
                 <svg
                 className="ml-1 size-5 animate-spin text-black"
@@ -25,7 +27,8 @@ export default function Login(){
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                 ></path>
                 </svg>
-            </main>
+            </main> */}
+            <SignInScreen />
             <Footer />
         </div>
     )
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,42 @@
 import {initializeApp} from 'firebase/app';
 import {getFirestore} from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-
+import 'firebaseui/dist/firebaseui.css'
 import { config } from "dotenv";
 config();
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCZodgmRgZsG-qgAZLm24J6q_dCS9Rm8Co",
+//   authDomain: "ratemyclasses-c70c2.firebaseapp.com",
+//   projectId: "ratemyclasses-c70c2",
+//   storageBucket: "ratemyclasses-c70c2.firebasestorage.app",
+//   messagingSenderId: "360721220097",
+//   appId: "1:360721220097:web:fc0f18b4c0e25e76a13600",
+//   measurementId: "G-2E6RF7X1M3"
+// };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
+const uiConfig = {
+    signInFlow: 'popup',
+    callbacks: {
+        signInSuccessWithAuthResult: () => false,
+    },
+    signInOptions: [
+        GoogleAuthProvider.PROVIDER_ID,
+    ],
+};
 
-export {db, app};
+export { db, app, auth, provider, uiConfig, firebaseConfig };
 
